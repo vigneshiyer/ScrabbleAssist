b25354523d4d5e702996c14956e50a2e257ee2c8
vigneshiyer <vignesh.iyer@gmail.com>
2016-07-05 06:42:01 +0530

    A sample input board and a test scrabble board class

--- ScrabbleConsole/src/board/ScrabbleBoard.java
+++ ScrabbleConsole/src/board/ScrabbleBoard.java
@@ -4,11 +4,26 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import dictionary.Dictionary;
+import java.util.Map;
+import java.util.HashMap;
 
 public class ScrabbleBoard {
 	public static void main(String[] args) throws IOException {
-		char[][] board = new char[15][15];
-		//Tile[][] board = new Tile[15][15];
+		char[][] input = new char[15][15];
+		Tile[][] board = new Tile[15][15];
+		
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				Tile tile = new Tile();
+				tile.setX(i);
+				tile.setY(j);
+				board[i][j] = tile;
+			}
+		}
+		
+		// key is a word on board, value is the Tile object of the starting letter of the word
+		Map<String,Tile> wordsOnBoard = new HashMap<String,Tile>();
+		
 		BufferedReader br = new BufferedReader(new FileReader("resources/words.txt"));
 		Dictionary dict = new Dictionary();
 		String str;
@@ -16,21 +31,78 @@ public class ScrabbleBoard {
 			dict.insert(str);
 		}
 		br.close();
+		
+		br = new BufferedReader(new FileReader("resources/input.txt"));
+		int row = 0;
+		while ((str = br.readLine()) != null) {
+			char[] arr = str.toCharArray();
+			for (int j = 0; j < arr.length; j++) {
+				input[row][j] = arr[j];
+			}
+			row++;
+		}
+		
+		// load horizontal words
+		/*StringBuilder sb = new StringBuilder();
+		int x = -1, y = -1;
+		for (int i = 0; i < input.length; i++) {
+			for (int j = 0; j < input.length; j++) {
+				if (input[i][j] != '.') {
+					if (x == -1 && y == -1) {
+						// word starts here
+						x = i;
+						y = j;
+					}
+					sb.append(input[i][j]);									
+				}
+				else {
+					if (sb.length() > 1) {
+						String word = sb.toString();
+						for (int k = 0; k < word.length(); k++) {
+							Tile tile = board[x][y+k];
+							tile.setHorizontalWord(word);
+						}
+					}
+					x = -1;
+					y = -1;
+					sb = new StringBuilder();
+				}
+			}
+			if (sb.length() > 1) {
+				String word = sb.toString();
+				for (int k = 0; k < word.length(); k++) {
+					Tile tile = board[x][y+k];
+					tile.setHorizontalWord(word);
+				}
+			}
+			x = -1;
+			y = -1;
+			sb = new StringBuilder();			
+		}*/
+		
 	}
 }
 
 class Tile {
 	private String horizontalWord, verticalWord;
+	private boolean isEmpty = true;
+	
+	public boolean isEmpty() {
+		return isEmpty;
+	}
+	
 	public String getHorizontalWord() {
 		return horizontalWord;
 	}
 	public void setHorizontalWord(String horizontalWord) {
 		this.horizontalWord = horizontalWord;
+		isEmpty = false;
 	}
 	public String getVerticalWord() {
 		return verticalWord;
 	}
 	public void setVerticalWord(String verticalWord) {
+		isEmpty = false;
 		this.verticalWord = verticalWord;
 	}
 	public int getPoint() {
